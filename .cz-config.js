77cd103b2929cac025a578741f47c6a5cccf045c
HyejinUm <uhj1993@naver.com>
2023-02-18 01:02:51 +0900

    🔨 refactor(config): cz-config 수정

--- .cz-config.js
+++ .cz-config.js
@@ -20,8 +20,44 @@ module.exports = {
       value: '🔧 chore',
     },
   ],
-  allowCustomScopes: false,
+  scopes: [
+    'component',
+    'css-style',
+    'custom-hook',
+    'store',
+    'util',
+    'api',
+
+    'wrong codes',
+    'spaghetti codes',
+    'alien codes',
+
+    'assets',
+    'package',
+
+    'lint',
+    'formatting',
+
+    'config',
+    'workflow',
+
+    // NOTE: .releaserc.js
+    'breaking',
+    'no-release',
+    'README',
+  ],
+  allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
   skipQuestions: ['body'],
   subjectLimit: 100,
+  messages: {
+    type: '커밋메시지의 타입을 설정해주세요:',
+    scope: '\n변경하려는 스코프는 무엇인가요? (optional):',
+    // used if allowCustomScopes is true
+    customScope: '변경하려는 스코프는 무엇인가요?:',
+    subject: '변화에 대한 짧고 간결한 설명을 적어주세요:\n',
+    body: '변화의 길고 자세한 설명을 적어주세요: (optional). "|"로 개행할 수 있어요:\n',
+    footer: '이 변화로 인해 ISSUES CLOSED으로 추가할 내용이 있나요? (optional). E.g.: #31, #34:\n',
+    confirmCommit: '이대로 커밋할까요?',
+  },
 };
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['gitmoji'],
   rules: {
+    'scope-case': [2, 'always', 'lower-case'],
     'header-max-length': [0, 'always', 100],
     'type-enum': [2, 'always', ['feat', 'fix', 'test', 'refactor', 'style', 'docs', 'chore']],
   },
