b491199597216bcb4d1b226b9be26b51ea02d4a6
paul950104@gmail.com <paul950104@gmail.com>
2023-02-23 02:12:04 +0900

    fix: router 수정중

--- wpreact/src/App.js
+++ wpreact/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter as NavLink, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as NavLink, Routes, Route, Router } from 'react-router-dom';
 import Home from './components/Home/Home';
 
 function App() {
   return (
     <div>
-        Hello World
+      <Router>
         <li>
           <NavLink to = "/">Home</NavLink>
         </li>
-        <Routes>
-          <Route path = "/" element = {<Home />}></Route>
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+      </Router>
     </div>
   );
 }
