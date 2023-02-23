e0838a2b3ab16961475c9c3d5650a67378c9aedd
JeongGeun Im <jg.im@mcorpor.com>
2023-02-23 12:17:37 +0900

    사이드바 Link 리스트 추가

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const SideBarSC = styled.aside`
 	width: 240px;
-	height: 100%;
+	min-height: 100%;
 	padding: 36px 48px;
 `;
 
+const NavListSC = styled.ul`
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+`;
+const NavListItemSC = styled(Link)`
+	padding: 12px 0;
+	font-size: 20px;
+`;
+
+const PAGES = [
+	{ path: '/page/1', name: 'PAGE A' },
+	{ path: '/page/2', name: 'PAGE B' },
+	{ path: '/page/3', name: 'PAGE C' },
+	{ path: '/page/4', name: 'PAGE D' },
+];
+
 const SideBar = (props) => {
-	return <SideBarSC>SideBar</SideBarSC>;
+	return (
+		<SideBarSC>
+			<NavListSC>
+				{PAGES.map((v, i) => (
+					<NavListItemSC
+						to={v.path}
+						key={v.name}
+					>
+						{v.name}
+					</NavListItemSC>
+				))}
+			</NavListSC>
+		</SideBarSC>
+	);
 };
 
 SideBar.propTypes = {};
