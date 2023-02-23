e6f38e436b57ce6f73fe2e4d91c9c7e33980ed22
huisu <huisu0429@naver.com>
2023-02-23 16:37:18 +0900

    login
    1. 로그인 유무 확인
    2. 로그인에 따라 페이지 다르게 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,11 @@
-import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
-import styled from 'styled-components';
-
-const Header = styled.header`
-  height: 100vh;
-  border-bottom: 1px black solid;
-  box-sizing: border-box;
-  height: 6vh;
-  padding: 15px;
-  font-size: 15px;
-  font-family: 'Roboto', sans-serif;
-  font-weight: 700;
-  font-style: italic;
-`;
-
-const Box = styled.div`
-  height: 94vh;
-  display: flex;
-`;
-
-const Nav = styled.div`
-  width: 200px;
-
-  border-right: 1px black solid;
-  text-align: center;
-  padding-top: 20px;
-`;
-
-const Li = styled.li`
-  list-style: none;
-  margin: 15px 0;
-  color: ${(props) => props.color};
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-`;
+import { useSelector } from 'react-redux';
+import Home from './Home';
+import Login from './Login';
 
 function App() {
-  const pathname = useLocation().pathname;
+  const isLogin = useSelector((state: any) => state.login.isLogin);
 
-  return (
-    <>
-      <Header>Wanted Pre-onboarding course</Header>
-      <Box>
-        <Nav>
-          <Li color={pathname === '/pageA' ? '#19769b' : 'black'}>
-            <Link to={'/pageA'}>Page A</Link>
-          </Li>
-          <Li color={pathname === '/pageB' ? '#19769b' : 'black'}>
-            <Link to={'/pageB'}>Page B</Link>
-          </Li>
-          <Li color={pathname === '/pageC' ? '#19769b' : 'black'}>
-            <Link to={'/pageC'}>Page C</Link>
-          </Li>
-        </Nav>
-        <Outlet />
-      </Box>
-    </>
-  );
+  return <>{isLogin ? <Home /> : <Login />}</>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,18 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
