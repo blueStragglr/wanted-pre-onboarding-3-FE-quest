5ba9ebf385e630b4a16b64b0c06d9087c96705cc
hyukzz <ri_chard@naver.com>
2023-02-24 19:54:38 +0900

    data: sidebar 목록 추가

---
+++ src/data/sidebar.ts
@@ -0,0 +1,7 @@
+const sidebar: { title: string; path: string; link: string }[] = [
+  { title: 'Page A', path: '/PageA', link: '/PageA' },
+  { title: 'Page B', path: '/PageB', link: '/PageB' },
+  { title: 'Page C', path: '/PageC', link: '/PageC' },
+];
+
+export default sidebar;
