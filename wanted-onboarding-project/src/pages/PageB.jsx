ab3bfc64d82a2fa4cb3215690f5847a69164b22a
jongik96 <pji3504@naver.com>
2023-02-24 23:07:57 +0900

    wanted frontend onboarding 사전과제 완료

---
+++ wanted-onboarding-project/src/pages/PageB.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PageB = () => {
+    return (<div style={{margin:'auto', color:'skyblue'}}>This is Page B!</div>)
+}
+
+export default PageB
---
+++ wanted-onboarding-project/src/pages/PageC.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PageC = () => {
+    return (<div style={{margin:'auto', color:'skyblue'}}>This is Page C!</div>)
+}
+
+export default PageC
