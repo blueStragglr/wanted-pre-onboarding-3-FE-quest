1d2bc1b5c474e6f91b6778b8bc6314963cf3dcd8
hyukzz <ri_chard@naver.com>
2023-02-24 19:57:58 +0900

    feat: route 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Layout from './layouts/layout/layout';
+import MainPage from './layouts/pages/MainPage';
+import PageA from './pages/PageA';
+import PageB from './pages/PageB';
+import PageC from './pages/PageC';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/pageA' element={<PageA />} />
+          <Route path='/pageB' element={<PageB />} />
+          <Route path='/pageC' element={<PageC />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
