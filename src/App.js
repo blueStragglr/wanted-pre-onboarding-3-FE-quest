a02667b724438dbb81398b3b834f04b55b6c3a19
letsjo <hyunoh.jo@gmail.com>
2023-02-26 02:27:24 +0900

    Feat: App init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import './App.css';
+import Header from 'components/layout/Header';
+import SideMenu from 'components/layout/SideMenu';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className='container'>
+      <Header />
+      <div className='mainSection'>
+        <SideMenu />
+        <div className='content'>
+          <h1>This is Page A!</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
