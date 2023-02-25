3cf0986cc2a63a6bb80357e09636eccbe1dc4f9e
Russ <yunsubin481@gmail.com>
2023-02-25 21:28:16 +0900

    market

---
+++ client/src/Pages/MarketPage.tsx
@@ -0,0 +1,17 @@
+import Footer from "../Components/Footer";
+
+const MarketPage =() => {
+
+    return(
+    <div className="marketpage">
+        <h1>MarketPage</h1>
+        <div className="NFT_1"></div>
+        <div className="NFT_1"></div>
+        <div className="NFT_1"></div>
+        <div className="NFT_1"></div>
+        <div className="NFT_1"></div>
+        <Footer/>
+    </div>
+    )
+}
+export default MarketPage;
