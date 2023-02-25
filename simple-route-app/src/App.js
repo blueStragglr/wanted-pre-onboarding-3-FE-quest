77eb922fcbeb9f64ea79e6d27e84cfa1d027be78
yeongi <cbg5255@gmail.com>
2023-02-25 16:58:11 +0900

    init : 프로젝트 생성 및 디렉토리 구조 변경

---
+++ simple-route-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Wanted Pre-onbording course : yeongi</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
