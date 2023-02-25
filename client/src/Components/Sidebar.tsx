162c9c2dddfd2c86f897f77e2d78135abb1c2293
Russ <yunsubin481@gmail.com>
2023-02-25 22:14:11 +0900

    sidebar

---
+++ client/src/Components/Sidebar.tsx
@@ -0,0 +1,40 @@
+const Sidebar =() => {
+
+    return(
+    <div className="sidebar">
+        <h1>Category</h1>
+
+
+
+
+
+
+
+
+
+        <div>
+            <h3>Ranker's Name</h3>
+        </div>
+        <div>
+            <h3>Memberships</h3>
+        </div>
+        <div>
+            <h3>Photography</h3>
+        </div>
+        <div>
+            <h3>Random Box</h3>
+        </div>
+        <div>
+            <h3>Gaming</h3>
+        </div>
+        <div>
+            <h3>PFPs</h3>
+        </div>
+        <div>
+            <h3>Art</h3>
+        </div>
+
+    </div>
+    )
+}
+export default Sidebar;
