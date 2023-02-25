fbbbbec819266d1d77f21e0c6df0e227d45d2376
Russ <yunsubin481@gmail.com>
2023-02-25 21:58:36 +0900

    MarketPage

--- client/src/Pages/MarketPage.tsx
+++ client/src/Pages/MarketPage.tsx
@@ -1,16 +1,21 @@
 import Footer from "../Components/Footer";
-
+import Sidebar from "../Components/Sidebar";
+import './MarketPage.scss'
 const MarketPage =() => {
 
     return(
     <div className="marketpage">
-        <h1>MarketPage</h1>
-        <div className="NFT_1"></div>
-        <div className="NFT_1"></div>
-        <div className="NFT_1"></div>
-        <div className="NFT_1"></div>
-        <div className="NFT_1"></div>
-        <Footer/>
+        <div className="marketpage_wrapper">
+            <Sidebar></Sidebar>
+            <div className="market">
+                <h1>MarketPage</h1>
+                <div className="NFT_1"></div>
+                <div className="NFT_1"></div>
+                <div className="NFT_1"></div>
+                <div className="NFT_1"></div>
+                <div className="NFT_1"></div>
+            </div>
+        </div>
     </div>
     )
 }
