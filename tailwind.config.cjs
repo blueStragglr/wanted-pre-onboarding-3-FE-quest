8444642d1ae7ae483136c95e123246d72084b468
thsuekfk2 <thsuekfk2@naver.com>
2023-02-26 02:22:11 +0900

    chore : install tailwind, setting config init

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      mainColor: "#2B3467",
+      subColor: "#BAD7E9",
+      bgColor: "#FCFFE7",
+      hoverColor: "#8f9db5",
+    },
+  },
+  plugins: [],
+};
