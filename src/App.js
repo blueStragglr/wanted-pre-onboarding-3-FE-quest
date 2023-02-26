ad2c678d8617620957c7d047bb8a2f8de16d4e6c
노혜지 <gpwl928@gmail.com>
2023-02-26 21:24:13 +0900

    feat: page들 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/index.js';
+import Main from './pages/Main/index.js';
+import PageA from './pages/PageA';
+import PageB from './pages/PageB';
+import PageC from './pages/PageC';
+import PageD from './pages/PageD';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Main />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route 
+            path="/pageA"
+            element={<PageA />}
+          />
+          <Route 
+            path="/pageB"
+            element={<PageB />}
+          />
+          <Route 
+            path="/pageC"
+            element={<PageC />}
+          />
+          <Route 
+            path="/pageD"
+            element={<PageD />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
