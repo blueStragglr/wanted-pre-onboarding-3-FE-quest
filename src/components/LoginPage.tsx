e6df2012f8da09716a2b58487728684bee0193ad
oyhoyhk <oyhoyhk@naver.com>
2023-02-26 10:12:25 +0900

    로그인 페이지 프레임 구현

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,5 +1,14 @@
 const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <div className="loginPage">
+      <form action="post">
+        <input type="text" placeholder="ID ..." />
+        <input type="password" placeholder="PW ..." />
+        <div className="errorDiv"></div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
