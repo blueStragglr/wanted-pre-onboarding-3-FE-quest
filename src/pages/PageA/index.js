86745174fb493d98ff9c6053e6f5cc145ddd9205
노혜지 <gpwl928@gmail.com>
2023-02-26 21:26:12 +0900

    feat: page A ~ D까지 추가

---
+++ src/pages/PageA/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import './PageA.css'
+
+const PageA = () => {
+  return (
+    <Layout>
+      <div className="page_container">
+        <h1 className="logo">여기는 A페이지 입니다</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default PageA;
---
+++ src/pages/PageC/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import './PageC.css'
+
+const PageC = () => {
+  return (
+    <Layout>
+      <div className="page_container">
+        <h1 className="logo">여기는 C페이지 입니다</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default PageC;
