1889a8f4ad83a637e7c53582e961c3491fb61c75
oyhoyhk <oyhoyhk@naver.com>
2023-02-26 10:58:01 +0900

    최종 제출

--- src/components/CustomNav.tsx
+++ src/components/CustomNav.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { navStyle } from "../style";
 
 const CustomNav = ({
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { mainStyle } from "../style";
 import CustomHeader from "./CustomHeader";
 import CustomNav from "./CustomNav";
