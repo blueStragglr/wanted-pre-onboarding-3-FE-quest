4514db24749cbd4296adef0b5dfa86c9b4119d6e
soohyuneee <tlstngus95@naver.com>
2023-02-27 23:03:09 +0900

    design : font color add

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0; 
         font-family: 'Space Mono';
+        color: #485460;
     } 
     button{
         display: flex;
