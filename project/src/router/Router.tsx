39f3014daa170c2539054d95aa444448eb88ed41
raon9401 <raon9401@naver.com>
2023-02-28 01:54:51 +0900

    feat : 기본 페이지 routing

--- project/src/router/Router.tsx
+++ project/src/router/Router.tsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { APage } from "../pages/APage";
+import { BPage } from "../pages/BPage";
+import { CPage } from "../pages/CPage";
+
 export const Router = () => {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<APage />} />
+        <Route path="/bpage" element={<BPage />} />
+        <Route path="/cpage" element={<CPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
