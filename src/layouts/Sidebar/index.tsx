29367735073696af4a1bdba4f04e28992a3ddada
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-27 13:37:39 +0900

    Fix Sidebar content

--- src/layouts/Sidebar/index.tsx
+++ src/layouts/Sidebar/index.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
 import * as S from "./style";
 
 const Sidebar = () => {
   return (
     <S.Wrapper>
-      <S.Content>사이드바 입니다</S.Content>
+      <Link to="/">
+        <S.Content>Page A</S.Content>
+      </Link>
+      <Link to="/b">
+        <S.Content>Page B</S.Content>
+      </Link>
+      <Link to="/c">
+        <S.Content>Page C</S.Content>
+      </Link>
     </S.Wrapper>
   );
 };
