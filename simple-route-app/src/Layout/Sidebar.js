3adba7e5a0120f7cbf64148472e15914cc474995
yeongi <cbg5255@gmail.com>
2023-02-27 19:27:07 +0900

    feat : uuid 사이드바 link key값에 추가

--- simple-route-app/src/Layout/Sidebar.js
+++ simple-route-app/src/Layout/Sidebar.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import styles from "./Layout.module.css";
-
+import { v4 } from "uuid";
 const Sidebar = ({ subRootList }) => {
   return (
     <>
       <nav className={styles["sidebar-wrapper"]}>
         {subRootList.map(({ path, linkName }) => {
           return (
-            <ul>
+            <ul key={v4()}>
               <Link to={path}>{linkName}</Link>
             </ul>
           );
