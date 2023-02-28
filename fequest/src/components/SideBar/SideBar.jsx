b13745c68743dac568987fdab85b7165dabdb174
HYE <jelly12paw@gmail.com>
2023-02-28 15:54:22 +0900

    chore: NavBar & SideBar link 연결

--- fequest/src/components/SideBar/SideBar.jsx
+++ fequest/src/components/SideBar/SideBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './SideBar.module.css'
 
 export default function SideBar() {
     return (
         <ul className={styles.ul}>
-            <li>Page A</li>
-            <li>Page B</li>
-            <li>Page C</li>
-            <li>Page D</li>
-            <li>Page E</li>
+            <li><Link to='/a' className={styles.link}>Page A</Link></li>
+            <li><Link to='/b' className={styles.link}>Page B</Link></li>
+            <li><Link to='/c' className={styles.link}>Page C</Link></li>
+            <li><Link to='/d' className={styles.link}>Page D</Link></li>
+            <li><Link to='/e' className={styles.link}>Page E</Link></li>
         </ul>
     );
 }
