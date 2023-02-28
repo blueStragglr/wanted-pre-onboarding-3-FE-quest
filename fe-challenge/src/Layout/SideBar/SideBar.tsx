9c1027fa75de5529ac2d06e8e4faaaa6a4f9dcbd
Jinnie <hjk7639@gmail.com>
2023-02-28 16:47:45 +0900

    [Feat] SideBar 메뉴 생성 & 페이지 클릭 시 해당 메뉴 버튼 컬러 변경

--- fe-challenge/src/Layout/SideBar/SideBar.tsx
+++ fe-challenge/src/Layout/SideBar/SideBar.tsx
@@ -1,16 +1,54 @@
 /** @jsxImportSource @emotion/react */
+import { Link, useLocation } from "react-router-dom";
 import { css } from "@emotion/react";
 
 const SideBar = () => {
+  const location = useLocation();
   return (
     <aside
       css={css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
         width: 250px;
         height: calc(100vh - 30px);
+        padding: 20px;
         border-right: 1px solid #000000;
         background-color: aliceblue;
       `}
-    ></aside>
+    >
+      <Link
+        to={"/pagea"}
+        css={css`
+          padding: 15px 50px;
+          border: 1px solid blueviolet;
+          ${location.pathname == "/pagea" && "background-color: pink"}
+        `}
+      >
+        Page A
+      </Link>
+      <Link
+        to={"/pageb"}
+        css={css`
+          padding: 15px 50px;
+          border: 1px solid blueviolet;
+          ${location.pathname == "/pageb" && "background-color: pink"}
+        `}
+      >
+        Page B
+      </Link>
+      <Link
+        to={"/pagec"}
+        css={css`
+          padding: 15px 50px;
+          border: 1px solid blueviolet;
+          ${location.pathname == "/pagec" && "background-color: pink"}
+        `}
+      >
+        Page C
+      </Link>
+    </aside>
   );
 };
 
