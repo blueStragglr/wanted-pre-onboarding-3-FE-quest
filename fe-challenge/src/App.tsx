696cdf97e9d98f1ebe9027d179285a6a778a8005
Jinnie <hjk7639@gmail.com>
2023-02-28 15:38:43 +0900

    [Feat] 기본적인 Header, Main 영역 레이아웃 구성

--- fe-challenge/src/App.tsx
+++ fe-challenge/src/App.tsx
@@ -1,10 +1,14 @@
 import reset from "./Style/style";
 import { Global } from "@emotion/react";
+import Header from "./Layout/Header/Header";
+import Mainlayout from "./Layout/MainLayout/MainLayout";
 
 function App() {
   return (
     <>
       <Global styles={reset} />
+      <Header />
+      <Mainlayout />
     </>
   );
 }
