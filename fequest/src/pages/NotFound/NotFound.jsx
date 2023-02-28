887fb40b39ee130dfd03dc1087b704aa9587ecd7
HYE <jelly12paw@gmail.com>
2023-02-28 15:18:54 +0900

    style: NotFound page

--- fequest/src/pages/NotFound/NotFound.jsx
+++ fequest/src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import styles from './NotFound.module.css'
 
 export default function NotFound() {
     return (
-        <div>
-            
+        <div className={styles.title}>
+            <div className={styles.text}>
+                Sorry! Page Not Found 😥
+            </div>
         </div>
     );
 }
