e15dbeb8ba9d326535428486a6532fbff2e33281
quokka-eating-carrots <ovzip7@gmail.com>
2023-02-28 18:29:40 +0900

    Feat: 라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { useState } from 'react';
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <SideBar />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
