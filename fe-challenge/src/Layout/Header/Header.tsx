7e38fac154884efd51a747a668e0f7f8774fd18d
Jinnie <hjk7639@gmail.com>
2023-02-28 20:39:57 +0900

    [Fix] 헤더에 로그인/로그아웃 버튼과 로직 추가

--- fe-challenge/src/Layout/Header/Header.tsx
+++ fe-challenge/src/Layout/Header/Header.tsx
@@ -1,10 +1,17 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [emailInfo, setEmailInfo] = useState(localStorage.getItem("email"));
+  const navigate = useNavigate();
+
   return (
     <header
       css={css`
+        display: flex;
+        justify-content: space-between;
         width: auto;
         height: 30px;
         border-bottom: 1px solid #000000;
@@ -18,6 +25,26 @@ const Header = () => {
       >
         Wanted Pre-onboarding course
       </h1>
+      <button
+        css={css`
+          width: 100px;
+          border: 1px solid #000000;
+          :hover {
+            background-color: aliceblue;
+          }
+        `}
+        onClick={() => {
+          if (emailInfo) {
+            localStorage.removeItem("email");
+            setEmailInfo(localStorage.getItem("email"));
+            navigate("/login");
+          } else {
+            navigate("/login");
+          }
+        }}
+      >
+        {emailInfo ? "Logout" : "login"}
+      </button>
     </header>
   );
 };
