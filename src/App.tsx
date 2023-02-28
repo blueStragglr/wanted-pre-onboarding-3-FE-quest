3125ef130c901e215441eb0a74d5ecc5535e494e
heony704 <heony704@gmail.com>
2023-02-28 21:25:33 +0900

    Chore: lint, format, route, style 관련 패키지 설치
    - axios
    - react-router-dom
    
    - eslint
    - prettier
    - husky
    
    - tailwindcss
    - react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>app</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
