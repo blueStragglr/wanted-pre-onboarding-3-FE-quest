e0fb64c75724ec049a301805a82465241596a859
Jinnie <hjk7639@gmail.com>
2023-02-28 15:32:40 +0900

    [Feat] Main 컴포넌트 생성

---
+++ fe-challenge/src/Layout/Main/Main.tsx
@@ -0,0 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+const Main = () => {
+  return (
+    <main
+      css={css`
+        width: 100%;
+        height: calc(100vh - 30px);
+        color: aquamarine;
+      `}
+    >
+      this is main
+    </main>
+  );
+};
+
+export default Main;
