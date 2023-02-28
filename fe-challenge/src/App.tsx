cbf14c576c601b3a61bf3c90862157cbb26b01ed
Jinnie <hjk7639@gmail.com>
2023-02-28 16:33:01 +0900

    [Feat] react router 적용

--- fe-challenge/src/App.tsx
+++ fe-challenge/src/App.tsx
@@ -1,14 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Mainlayout from "./Layout/MainLayout/MainLayout";
+import PageA from "./Pages/PageA";
+import PageB from "./Pages/PageB";
+import PageC from "./Pages/PageC";
 import reset from "./Style/style";
 import { Global } from "@emotion/react";
-import Header from "./Layout/Header/Header";
-import Mainlayout from "./Layout/MainLayout/MainLayout";
 
 function App() {
   return (
     <>
       <Global styles={reset} />
-      <Header />
-      <Mainlayout />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Mainlayout />}>
+            <Route path={"/"} element={<PageA />} />
+            <Route path={"/pagea"} element={<PageA />} />
+            <Route path={"/pageb"} element={<PageB />} />
+            <Route path={"/pagec"} element={<PageC />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
