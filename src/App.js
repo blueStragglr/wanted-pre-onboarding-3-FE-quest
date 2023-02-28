7d5607193a42b49c6ff3b53cf71b4cc828c8b59e
yongseongjeon <yongseong.jeon95@gmail.com>
2023-02-28 16:11:26 +0900

    feat: apply router that utilizes ROUTES

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import A from "./pages/A";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ROUTES from "./routes";
 import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <A />
+      <BrowserRouter>
+        <Routes>
+          {ROUTES.map(({ path, element }) => (
+            <Route path={path} element={element} />
+          ))}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
