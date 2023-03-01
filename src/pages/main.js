10e15e9633e4361ef70996975f45d6a2027b42a4
who0803 <10rlatkdtn@naver.com>
2023-03-02 02:10:44 +0900

    feat: Create main, signin pages in pages folder
    - pages 폴더 생성
    - main 페이지 생성, sigin 페이지 생성

---
+++ src/pages/main.js
@@ -0,0 +1,7 @@
+export const Main = () => {
+  return (
+    <div>
+      메인 페이지입니다.
+    </div>
+  )
+}
---
+++ src/pages/signin.js
@@ -0,0 +1,7 @@
+export const Signin = () => {
+  return (
+    <div>
+      로그인 페이지입니다.
+    </div>
+  )
+}
