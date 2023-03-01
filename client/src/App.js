e541a15f7aa791994b37ca776b9a7fb0f1a4e5fa
lmimoh <dlalsgns9039@gmail.com>
2023-03-01 22:55:19 +0900

    Style : css 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import styled from "styled-components";
 import PageA from "./pages/PageA";
 import PageB from "./pages/PageB";
 import PageC from "./pages/PageC";
 import Header from "./components/public/Header";
 import SideBar from "./components/public/SideBar";
 
+const Wrapper = styled.div`
+  display: flex;
+  height: calc(100% - 80px);
+`
+
 function App() {
   return (
-    <main>
-      <BrowserRouter>
-        <Header />
+    <BrowserRouter>
+      <Header />
+      <Wrapper>
         <SideBar />
         <Routes>
           <Route path="/" element={<PageA />} />
           <Route path="/b" element={<PageB />} />
           <Route path="/c" element={<PageC />} />
         </Routes>
-      </BrowserRouter>
-    </main>
+      </Wrapper>
+    </BrowserRouter>
   );
 }
 
