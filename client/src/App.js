9096483e2ef87073f540a4d248353f7a5ead07d1
lmimoh <dlalsgns9039@gmail.com>
2023-03-01 15:34:04 +0900

    Feat : set react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import PageA from "./pages/PageA";
+import PageB from "./pages/PageB";
+import PageC from "./pages/PageC";
+import Header from "./components/public/Header";
+import SideBar from "./components/public/SideBar";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main>
+      <BrowserRouter>
+        <Header />
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<PageA />} />
+          <Route path="/b" element={<PageB />} />
+          <Route path="/c" element={<PageC />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
