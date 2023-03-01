ba5e62b62cee8dfd476c9f2406c636f0c6ba0860
soralee <sora2821@gmail.com>
2023-03-01 18:36:26 +0900

    Feat: add Page A, B, C components

---
+++ src/pages/PageA/index.tsx
@@ -0,0 +1,7 @@
+export const PageA = () => {
+  return (
+    <section className="basis-5/6">
+      <h1 className="text-sky-500 m-3 font-semibold">Page A</h1>
+    </section>
+  );
+};
---
+++ src/pages/PageC/index.tsx
@@ -0,0 +1,7 @@
+export const PageC = () => {
+  return (
+    <section className="basis-5/6">
+      <h1 className="text-sky-500 m-3 font-semibold">Page C</h1>
+    </section>
+  );
+};
