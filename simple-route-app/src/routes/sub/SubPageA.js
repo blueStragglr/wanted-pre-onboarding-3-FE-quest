d4e2192d68ef0a4a755c30840d1741383682701f
yeongi <cbg5255@gmail.com>
2023-03-01 14:13:31 +0900

    feat :임시 로그인 상태 판별

--- simple-route-app/src/routes/sub/SubPageA.js
+++ simple-route-app/src/routes/sub/SubPageA.js
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
+import styles from "./subPage.module.css";
+
 const SubPageA = (props) => {
+  const [login] = useState(false);
+
+  useEffect(() => {
+    //로그인 여부를 상태관리 라이브러리로 확인
+  }, []);
+
   return (
-    <>
-      <h1> A 페이지 입니다.</h1>
-    </>
+    <div className={styles.wrapper}>
+      {login && <h1> A 페이지 입니다. </h1>}
+      {!login && <h1> 로그인이 필요합니다. </h1>}
+    </div>
   );
 };
 
