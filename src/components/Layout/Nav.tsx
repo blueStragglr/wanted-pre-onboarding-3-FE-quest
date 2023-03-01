806e7867e74853048818b8b6b1eb64400a449873
sena <senika_@naver.com>
2023-03-01 11:28:18 +0900

    #2 nav setting

--- src/components/Layout/Nav.tsx
+++ src/components/Layout/Nav.tsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <>
+      <div>
+        <Link to="page1">A</Link>
+      </div>
+      <div>
+        <Link to="page2">B</Link>
+      </div>
+      <div>
+        <Link to="page3">C</Link>
+      </div>
+    </>
+  );
 };
 
 export default Nav;
