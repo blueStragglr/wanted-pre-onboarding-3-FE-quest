ac79c0e098d245e2ba628b63af6b72c7b50ac176
yeonkyoung <keep.ykk@gmail.com>
2023-03-01 16:24:03 +0900

    [#3] css module 파일 추가 (#4)
    * Style: Contents css 추가
    
    * Style: Header css 추가
    
    * Style: NavBar css 추가
    
    * Style: PageButton css 추가
    
    * Feat: 느낌표를 나타내는 상수 추가

--- src/constant/constant.tsx
+++ src/constant/constant.tsx
@@ -1,3 +1,4 @@
 export const HEADER_LABEL = 'Wanted Pre-onboarding course';
 export const NAV_PAGE_LABEL = 'Page';
 export const ARTICLE_LABEL = 'This is Page ';
+export const EXCLAMATION_MARK = '!';
