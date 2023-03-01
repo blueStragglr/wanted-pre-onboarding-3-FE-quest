433ae7c4882ccd08161d4707d362bafa0bc358c9
sak5010 <sak5010@naver.com>
2023-03-02 00:03:31 +0900

    styling: 헤더, 사이드바 구조, 리스트 하이라이팅

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <header>
-      <h4>Wanted Pre-onboarding course</h4>
+      <h4 className={styles.h4}>Wanted Pre-onboarding course</h4>
       <hr />
     </header>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
+import Sidbar from "../components/Sidebar";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <>
+    <div className={styles.container}>
       <Header />
+      <Sidbar />
       <div>Hello, there!</div>
-    </>
+    </div>
   );
 };
 
