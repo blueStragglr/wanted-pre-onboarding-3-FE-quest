e003ba12411e05f47c1886f7e51d348f8a548f17
lmimoh <dlalsgns9039@gmail.com>
2023-03-01 22:53:16 +0900

    Feat : curPage에 따른 별도의 색상 렌더링 & Link 세팅

--- client/src/components/public/SideBar.jsx
+++ client/src/components/public/SideBar.jsx
@@ -1,12 +1,38 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import styled from "styled-components";
+import { useLocation, Link } from "react-router-dom";
+
+const StyledSideBar = styled.aside`
+  border-right: 1px solid #DBDBDB;
+  width: 200px;
+  padding: 20px 0px 0px 20px;
+
+  nav {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .located {
+    color: blue;
+  }
+`
 
 const SideBar = () => {
+  const [ curPage, setCurPage ] = useState(useLocation().pathname);
+
+  const handleCurPage = (page) => {
+    setCurPage(page);
+  }
+
   return (
-    <aside>
-      <Link to={"/"}>A</Link>
-      <Link to={"/b"}>B</Link>
-      <Link to={"/c"}>C</Link>
-    </aside>
+    <StyledSideBar>
+      <nav>
+        <Link to={"/"} className={curPage === "/" && "located"} onClick={() => handleCurPage("/")}>Page A</Link>
+        <Link to={"/b"} className={curPage === "/b" && "located"} onClick={() => handleCurPage("/b")}>Page B</Link>
+        <Link to={"/c"} className={curPage === "/c" && "located"} onClick={() => handleCurPage("/c")}>Page C</Link>
+      </nav>
+    </StyledSideBar>
   )
 }
 
