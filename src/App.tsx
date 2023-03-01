1b44cc8ff7d1957366aee0c87a2071384e7aae91
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-03-01 11:47:34 +0900

    feat: 페이지에 따른 기본적인 routing 설정 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+	Route,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<>
+			<Route path="/" element={<LoginPage />} />
+			<Route
+				path="/page/:id"
+				element={<HomePage />}
+				errorElement={<NotFound />}
+			/>
+		</>
+	)
+);
 
 function App() {
-	return <div className="App">initialSetting</div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
