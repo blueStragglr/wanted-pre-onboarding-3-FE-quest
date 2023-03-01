8b2bbb57c04da95b33f67b36d61751ff033d2349
Kenna <kenna.hwa@gmail.com>
2023-03-01 20:22:44 +0900

    Design: header 섹션 CSS 수정

--- wanted_login_app/src/components/layout/Header.js
+++ wanted_login_app/src/components/layout/Header.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<header className="flex w-full m0 bg-red-100 " >
-			<h1 style={{ margin: 0 }}>Wanted Pre-onboarding course Login app</h1>
+		<header className="flex flex-nowrap items-center	justify-between	 w-full m0 bg-blue-10 py-5 px-3 border-b-1 border-gray-500" >
+			<h1 className="text-lg	font-bold">Wanted Pre-onboarding course Login app</h1>
 			<section className="login-section">
-					<button className="h-10 px-6 font-bold rounded-md bg-black text-white" type="button">Login</button>
+					<Link className="h-5 px-5 py-2 font-bold rounded-md bg-black  text-white" to="/login">Login</Link>
 			</section>
 		</header>
 	)
