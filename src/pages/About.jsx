9163adf50ce76bae125fb863deeb5e3446ccc723
jisooround <9utesleben@gmail.com>
2023-03-01 12:16:03 +0900

    Design: 스타일 추가

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
 const About = () => {
-  return <div className="page">About</div>;
+  return (
+    <Page className="page">
+      <p>About</p>
+    </Page>
+  );
 };
+
+const Page = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 60px;
+`;
 
 export default About;
