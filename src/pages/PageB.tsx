735508f484f7ad1065190e84f30f3d10f04bc1e4
Hayeon Baek <bhy0512@gmail.com>
2023-03-01 22:59:20 +0900

    👔  Update PageB and PageC

--- src/pages/PageB.tsx
+++ src/pages/PageB.tsx
@@ -1,3 +1,17 @@
 export const PageB = () => {
-  return <h1>This is Page B!</h1>;
+  const isAuth = localStorage.getItem('user');
+  console.log(isAuth);
+
+  return (
+    <>
+      {isAuth ? (
+        <h1>This is Page B!</h1>
+      ) : (
+        <>
+          <h1>Page B requires login to access! Please log in first.</h1>
+          <a href='/login' className='login'>login</a>
+        </>
+      )}
+    </>
+  );
 };
--- src/pages/PageC.tsx
+++ src/pages/PageC.tsx
@@ -1,3 +1,18 @@
 export const PageC = () => {
-  return <h1>This is Page C!</h1>;
+  const isAuth = localStorage.getItem('user');
+
+  return (
+    <>
+      {isAuth ? (
+        <h1>This is Page C!</h1>
+      ) : (
+        <>
+          <h1>Page C requires login to access! Please log in first.</h1>
+          <a href='/login' className='login'>
+            login
+          </a>
+        </>
+      )}
+    </>
+  );
 };
