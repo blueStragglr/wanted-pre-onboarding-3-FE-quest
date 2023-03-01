70389bdc4181ab02343e577673d5ccce76de599f
Kenna <kenna.hwa@gmail.com>
2023-03-01 20:32:08 +0900

    Design: Login 컴포넌트 CSS 수정

--- wanted_login_app/src/components/layout/DrawNav.js
+++ wanted_login_app/src/components/layout/DrawNav.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 const DrawNav = () => {
 
 	return (
-		<nav role="navigation" className="flex flex-col w-48 h-full bg-sky-100 relative">
-			<ul className="relative w-full">
-				<li><Link title="메인Home" to="/" >메인Home</Link></li>
-				<li><Link title="인트로Intro" to="/intro" >인트로Intro</Link></li>
-				<li><Link title="로그인Login" to="/login" >로그인Login</Link></li>
+		<nav role="navigation" className="flex flex-col w-60 h-full px-2 py-5 bg-blue-5 relative">
+			<ul className="relative w-full flex flex-col  flex-nowrap items-center	justify-center font-bold	 ">
+				<li className="mb-2"><Link className="text-base hover:text-orange-300" title="메인Home" to="/" >메인Home</Link></li>
+				<li className="mb-2"><Link className="text-base hover:text-orange-300" title="인트로Intro" to="/intro" >인트로Intro</Link></li>
+				<li className="mb-2"><Link className="text-base hover:text-orange-300" title="로그인Login" to="/login" >로그인Login</Link></li>
 
 			</ul>
 		</nav>
