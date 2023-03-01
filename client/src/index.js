0f6ed8a598dc30f3ac8120ac0bd432031b8db5ed
lmimoh <dlalsgns9039@gmail.com>
2023-03-01 22:49:24 +0900

    Style : create global style

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+    }
+
+    #root {
+        height: 100vh;
+    }
+
+    a {
+        text-decoration: none;
+        font-size: 12pt;
+        font-weight: 600;
+        color: gray;
+    }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <>
+        <GlobalStyle />
+        <App />
+    </>
 );
