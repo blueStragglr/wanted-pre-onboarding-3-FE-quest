84518f70436d81dce6f3df8390408f06967283d6
DESKTOP-K80COT4\love7 <mochyn404@gmail.com>
2023-03-02 22:35:01 +0900

    깃 저장

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import Header from './Header';
+import SideWrap from './Side';
+import Page from './Page';
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
+  let [nowPage] = useState([1]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='body'>
+        <Header />
+        <div className='contents'>
+          <SideWrap />
+          <div className='pageWwrap'>
+            <Route exact path="/">
+              <Page name="This is Page A!" />
+            </Route>
+            <Route path="/b">
+              <Page name="This is Page B!" />
+            </Route>
+            <Route path="/c">
+              <Page name="This is Page C!" />
+            </Route>
+
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
