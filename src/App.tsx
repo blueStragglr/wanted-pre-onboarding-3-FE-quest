104e5c8e83b392fa475905159389513b4e0f3790
Yena-Yun <cheryleduart@gmail.com>
2023-03-02 13:18:01 +0900

    react-router-dom 설치, App.tsx에 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from 'pages/Home/Home';
+import { Login } from 'pages/Login/Login';
+
 function App() {
-  return <div className='App'>시작!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
