251a1c2e128e946533939831bde6a5f69e061df3
kmonguu <skdus8009@gmail.com>
2023-03-02 21:46:50 +0900

    feat: page routing 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Global } from "@emotion/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import globalCss from "./global";
+import PageA from "./pages/PageA";
+import PageB from "./pages/PageB";
+import PageC from "./pages/PageC";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <>
+      <Global styles={globalCss}></Global>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/page-a" element={<PageA />}></Route>
+            <Route path="/page-b" element={<PageB />}></Route>
+            <Route path="/page-c" element={<PageC />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
