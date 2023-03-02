d232e38b7d31ecd9e9c199f266b5b4a42d42b65c
kkyo4994 <kkyo8747@naver.com>
2023-03-02 16:02:49 +0900

    [SETTING] 불필요한 파일들 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      3월 원티드 프리온보딩
     </div>
   );
 }
