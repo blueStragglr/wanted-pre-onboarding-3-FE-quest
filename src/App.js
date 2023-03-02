9615953f7ca232c1c82fa62009f8093e14d9eb94
ParkHyunKyeong <kj3477417@gmail.com>
2023-03-02 21:49:27 +0900

    refactor: App.js에서 미사용 import 구문 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Template from './components/Template';
 import GlobalStyle from './styles/GlobalStyle';
 import PageA from './pages/PageA';
 
