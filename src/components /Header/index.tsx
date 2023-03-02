456a4687f62f121751beb9f4be9e56fd81fdd58f
kkyo4994 <kkyo8747@naver.com>
2023-03-02 23:46:11 +0900

    [FEAT] 로그아웃 처리

--- src/components /Header/index.tsx
+++ src/components /Header/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 import * as S from './styles';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const onClickLogoutBtn = () => {
+    navigate('/');
+    localStorage.removeItem('userName');
+    toast.success('로그아웃이 되었습니다.');
+  };
   return (
     <S.HeaderArea>
       <S.HeaderText>Wanted Pre-onboarding course</S.HeaderText>
-      <S.LogoutBtn>Logout</S.LogoutBtn>
+      <S.LogoutBtn onClick={onClickLogoutBtn}>Logout</S.LogoutBtn>
     </S.HeaderArea>
   );
 };
