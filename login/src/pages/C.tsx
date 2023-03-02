75351f2405f54cef1a7d3b4a72f0093e6b0dc139
seungb5 <seungbo1112@gmail.com>
2023-03-02 21:19:15 +0900

    fix: colorType

--- login/src/pages/C.tsx
+++ login/src/pages/C.tsx
@@ -3,7 +3,7 @@ import { colorType } from "../types/colorType";
 export default function C(): JSX.Element {
   return (
     <>
-      <p style={{ color: `${colorType.isActive}` }}> This is Page C!</p>
+      <p style={{ color: `${colorType.event.isActive}` }}> This is Page C!</p>
     </>
   );
 }
