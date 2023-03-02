139870477092376bd8c87495320b0c778feb1b30
Yena-Yun <cheryleduart@gmail.com>
2023-03-02 12:35:07 +0900

    declare.d.ts 생성, scss/svg/png 확장자 셋팅

---
+++ declare.d.ts
@@ -0,0 +1,11 @@
+declare module '*.scss';
+declare module '*.svg';
+declare module '*.png';
+
+declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
