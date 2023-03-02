c408422e167a8316d17ed74795de315a9f45fdd2
Yena-Yun <cheryleduart@gmail.com>
2023-03-02 18:09:40 +0900

    Home.tsx에 Default 컴포넌트 연결

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,11 @@
 import { Layout } from 'components/Layout/Layout';
+import { Default } from 'components/Content/Default/Default';
 import styles from './Home.module.scss';
 
 export const Home = () => {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <Default />
+    </Layout>
+  );
 };
