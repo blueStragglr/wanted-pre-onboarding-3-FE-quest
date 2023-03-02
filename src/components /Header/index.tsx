b4bcb75d40fd4f38f38ff77e307507c496376307
kkyo4994 <kkyo8747@naver.com>
2023-03-02 19:51:01 +0900

    [FEAT] Header 퍼블리싱 완성

---
+++ src/components /Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './styles';
+
+const Header = () => {
+  return (
+    <S.HeaderArea>
+      <S.HeaderText>Wanted Pre-onboarding course</S.HeaderText>
+      <S.LogoutBtn>Logout</S.LogoutBtn>
+    </S.HeaderArea>
+  );
+};
+
+export default Header;
---
+++ src/components /Header/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const HeaderArea = styled.div`
+  height: 80px;
+  border-bottom: 1px solid #9f9f9f;
+  padding: 20px 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderText = styled.span`
+  font-size: 1.5rem;
+  font-style: italic;
+`;
+
+export const LogoutBtn = styled.button`
+  width: 80px;
+  height: 100%;
+  background-color: #1d78b6;
+  border: 1px solid #9f9f9f;
+  color: #ffffff;
+  cursor: pointer;
+`;
