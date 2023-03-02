242dd05c33dfb93d2ab487ece5a8d18778139037
jiwonmik <jiwonmik@gmail.com>
2023-03-02 22:42:04 +0900

    chores: initial settings with Theme

---
+++ src/styles/theme.tsx
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  bgColor: 'white',
+  textColor: 'black',
+  highlight: '#0A73BC',
+  headerFont: "'Roboto', sans-serif"
+};
