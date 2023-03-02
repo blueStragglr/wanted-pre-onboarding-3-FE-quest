ae70a8a9c6f324545872d2f5cd0ca90c34f5aca7
Yena-Yun <cheryleduart@gmail.com>
2023-03-02 15:56:29 +0900

    Layout.tsx에 Header, Sidebar 컴포넌트 셋팅

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,21 @@
+import { ReactNode } from 'react';
+import { Header } from './Header/Header';
+import { Sidebar } from './Sidebar/Sidebar';
 import styles from './Layout.module.scss';
 
-export const Layout = () => {
-  return <div>Layout</div>;
+type LayoutType = {
+  children: ReactNode | ReactNode[];
+};
+
+export const Layout = ({ children }: LayoutType) => {
+  return (
+    <div className={styles.container}>
+      <Header />
+
+      <div className={styles.mainContainer}>
+        <Sidebar />
+        {children}
+      </div>
+    </div>
+  );
 };
