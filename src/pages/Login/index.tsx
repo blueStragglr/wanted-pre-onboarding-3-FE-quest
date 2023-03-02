4e40cbbbe1fbd44fd47092c06dc72f40cc654c20
kkyo4994 <kkyo8747@naver.com>
2023-03-02 23:47:17 +0900

    [FEAT] Pages(로그인, 메인) 생성

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,7 @@
+import Login from '../../components /Login';
+
+const LoginPage = () => {
+  return <Login />;
+};
+
+export default LoginPage;
---
+++ src/pages/index.ts
@@ -0,0 +1,2 @@
+export { default as MainPage } from './Main';
+export { default as LoginPage } from './Login';
