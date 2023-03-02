561995702f791c9988ff8a3b454e29fd4666c2d0
ParkHyunKyeong <kj3477417@gmail.com>
2023-03-03 01:54:58 +0900

    design: material ui를 활용해 로그인 페이지 디자인 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const Login = () => {
   const [isSubmit, setIsSubmit] = useState(false);
@@ -25,9 +27,17 @@ const Login = () => {
     <LoginContainer>
       <LoginWrapper>
         <Title>로그인 페이지</Title>
-        <UserName placeholder="유저 이름" onChange={onChangeUserName} />
-        <Password placeholder="비밀번호" onChange={onChangePassword} />
-        <LoginButton onClick={onClickLogin}>로그인</LoginButton>
+        <UserNameWrapper>
+          <UserName label="유저네임" type="text" onChange={onChangeUserName} />
+        </UserNameWrapper>
+        <PasswordWrapper>
+          <Password label="비밀번호" type="password" helperText="최소 8 자, 최소 하나의 문자 및 하나의 숫자" onChange={onChangePassword} />
+        </PasswordWrapper>
+        <ButtonWrapper>
+          <LoginButton variant="contained" onClick={onClickLogin}>
+            로그인
+          </LoginButton>
+        </ButtonWrapper>
       </LoginWrapper>
     </LoginContainer>
   );
@@ -38,20 +48,55 @@ export default Login;
 const LoginContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: relative;
 `;
 
-const LoginWrapper = styled.div``;
+const LoginWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 const Title = styled.div`
-  font-size: 24px;
+  margin-bottom: 50px;
+  font-size: 36px;
+  font-weight: bold;
   color: royalblue;
+  text-align: center;
 `;
 
-const UserName = styled.input``;
+const UserNameWrapper = styled.div`
+  margin: 20px 0px;
+`;
 
-const Password = styled.input``;
+const PasswordWrapper = styled.div`
+  margin: 20px 0px;
+`;
 
-const LoginButton = styled.button``;
+const ButtonWrapper = styled.div`
+  margin: 20px 0px;
+  float: right;
+`;
+
+const LoginBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  & + & {
+    margin: 10px 0px;
+  }
+`;
+
+const UserName = styled(TextField)`
+  display: block;
+  width: 300px;
+`;
+
+const Password = styled(TextField)`
+  display: block;
+  width: 300px;
+`;
+
+const LoginButton = styled(Button)`
+  width: 100px;
+`;
