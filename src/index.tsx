f5cee4ed04c7fe1e8f0c6d634f9cf54a401d5f6b
Yena-Yun <cheryleduart@gmail.com>
2023-03-02 12:53:30 +0900

    import문 파일명 수정: globals.scss -> global.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/globals.scss';
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
