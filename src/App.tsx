edf125dcf2492508b129577a856ec2a792ba6b0d
Yena-Yun <cheryleduart@gmail.com>
2023-03-02 18:03:46 +0900

    App.tsx 중첩 라우팅으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from 'pages/Home/Home';
 import { Login } from 'pages/Login/Login';
+import { Default } from 'components/Content/Default/Default';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/*' element={<Home />}>
+          <Route path=':id' element={<Default />} />
+        </Route>
         <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
