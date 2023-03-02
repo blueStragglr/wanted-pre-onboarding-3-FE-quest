cec120d2f26678a0f0b0d924d02c1c4db238b9a7
ParkHyunKyeong <kj3477417@gmail.com>
2023-03-03 00:16:44 +0900

    feature: 라우터에 PageB, PageC 등록 및 기본 페이지를 PageA로 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import PageA from './pages/PageA';
+import PageB from './pages/PageB';
+import PageC from './pages/PageC';
 
 function App() {
   return (
     <React.Fragment>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<PageA />}></Route>
+        <Route path="/" element={<Navigate replace to="/page/a" />} />
+        <Route path="/page/a" element={<PageA />}></Route>
+        <Route path="/page/b" element={<PageB />}></Route>
+        <Route path="/page/c" element={<PageC />}></Route>
+        <Route path="*" element={<Navigate replace to="/page/a" />} />
       </Routes>
     </React.Fragment>
   );
