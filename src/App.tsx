9a24b7b668bb5a277b03a787bfe94eab938630cd
hyorimm <hyorimm@gmail.com>
2023-03-02 14:39:20 +0900

    feat: 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
+import A from './pages/A';
+import B from './pages/B';
+import Home from './pages/Home';
 
 function App() {
-  return <div className='text-blue-500'>안녕하세요</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index path='/' element={<Home />} />
+          <Route path='/a' element={<A />} />
+          <Route path='/b' element={<B />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
