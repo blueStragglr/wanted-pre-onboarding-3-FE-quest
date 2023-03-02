422a96eb21b3e87e24e3dcc47e05da9f882c413e
yunsunji9 <yunsunji9@hanmail.net>
2023-03-02 14:21:36 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
