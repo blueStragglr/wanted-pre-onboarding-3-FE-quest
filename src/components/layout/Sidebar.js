d278f7f4ac40b9f1d82b229c9ad327432d0e4778
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-03-02 16:20:26 +0900

    feat: 레이아웃 구현

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return <Wrap>Sidebar</Wrap>;
 };
+
+const Wrap = styled.div`
+  padding: 20px;
+  box-sizing: border-box;
+  height: 100%;
+  width: 250px;
+  border-right: 1px solid grey;
+`;
 
 export default Sidebar;
