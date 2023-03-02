163f324dc5b9d24cd2d95312f2b445be6be630c3
who0803 <10rlatkdtn@naver.com>
2023-03-02 21:37:49 +0900

    feat: Implement nested routing
    - 중첩 라우팅 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 import { Main, Signin } from './pages';
+import { Header, Content, Nav } from "./components"
+
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={ <Main/> } />
+        <Route path="/" element={ <Main/> }>
+          <Route path="/" element={ <Content/> }/>
+          <Route path="/A" element={ <Content text={'A'}/> }/>
+          <Route path="/B" element={ <Content text={'B'}/> }/>
+          <Route path="/C" element={ <Content text={'C'}/> }/>
+        </Route>
         <Route path="/signin" element={ <Signin/> } />
+        <Route path="/*" element={ <div>404</div> } />
       </Routes>
     </>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export * from './header';
-// export * from './nav';
+export * from './nav';
+export * from './content';
