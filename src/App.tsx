b5e14d27eaaaada7484d72db878ec439d56ede7d
ukheon <dnrgjs33@naver.com>
2023-03-03 17:11:56 +0900

    init: create vite 사용하여 환경설정

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+    const myGenericFunction = <T,>(arg: T): T => {
+        return arg;
+    }
+    return (
+        <div className="App">
+          
+        </div>
+    );
+}
+
+export default App;
