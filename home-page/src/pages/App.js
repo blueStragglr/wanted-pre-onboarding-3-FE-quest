a77d09ca4e2f9abf0d4267bef2e43ebde053010b
hyesilHeo <dmsgktn1414@gmail.com>
2023-03-03 23:57:15 +0900

    fix: 페이지D 구현

--- home-page/src/pages/App.js
+++ home-page/src/pages/App.js
@@ -5,6 +5,7 @@ import HomeContainer from "../components/HomeContainer";
 import PageA from "./PageA";
 import PageB from "./PageB";
 import PageC from "./PageC";
+import PageD from "./PageD";
 import Login from "./Login";
 import NotFound from "./NotFound";
 
@@ -19,6 +20,7 @@ const App = () => {
             <Route path="/pageA" element={<PageA />} />
             <Route path="/PageB" element={<PageB />} />
             <Route path="/PageC" element={<PageC />} />
+            <Route path="/PageD" element={<PageD />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
--- home-page/src/pages/index.js
+++ home-page/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "./pages/App";
+import App from "./App";
 import { createRoot } from "react-dom/client";
 
 const rootElement = document.getElementById("root");
