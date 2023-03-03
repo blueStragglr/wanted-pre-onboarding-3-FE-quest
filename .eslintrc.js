6b2a366e52ea01f637b1a00d62ef2d16716d6c48
sunnao <osa125125@naver.com>
2023-03-03 17:33:15 +0900

    ✨ feat(sfdsf): ➕ dependency: 의존성추가 및 eslint 오류 해결

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,26 @@ module.exports = {
     'import/prefer-default-export': ['off'],
     'react-hooks/exhaustive-deps': ['warn'],
     'react/jsx-props-no-spreading': [1, { custom: 'ignore' }],
+    'react/function-component-definition': [2, { namedComponents: ['arrow-function', 'function-declaration'] }],
+    'import/no-unresolved': 'off',
+    'import/extensions': [
+      'off',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        mjs: 'never',
+      },
+    ],
+
+    // '@typescript-eslint/explicit-function-return-type': ['off'],
     settings: {
+      'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
       'import/resolver': {
         node: {
           extensions: ['.js', '.jsx', '.ts', '.tsx'],
