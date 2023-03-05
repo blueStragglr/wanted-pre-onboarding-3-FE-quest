2f1be85a419497162bb5f476bfb1f2ad74401aba
yunho <kimyouknow@naver.com>
2023-03-05 15:56:39 +0900

    Refactor: GNB_LINK 배열 생성

--- client/src/components/GlobalNavigation.tsx
+++ client/src/components/GlobalNavigation.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom'
 
-import { ROUTE } from '@/constants/route.constant'
+import { GNB_LINK } from '@/constants/route.constant'
 
 interface GlobalNavigationProps {
   className?: string
@@ -10,31 +10,13 @@ const GlobalNavigation = ({ className }: GlobalNavigationProps) => {
   return (
     <nav className={className}>
       <ul className="flex flex-col gap-4">
-        <li className="w-24 font-semibold text-gray-800 hover:text-blue-500">
-          <NavLink to={ROUTE.HOME} className={({ isActive }) => (isActive ? 'text-blue-500' : '')}>
-            HOME
-          </NavLink>
-        </li>
-        <li className="w-24 font-semibold text-gray-800 hover:text-blue-500">
-          <NavLink to={ROUTE.A} className={({ isActive }) => (isActive ? 'text-blue-500' : '')}>
-            A
-          </NavLink>
-        </li>
-        <li className="w-24 font-semibold text-gray-800 hover:text-blue-500">
-          <NavLink to={ROUTE.B} className={({ isActive }) => (isActive ? 'text-blue-500' : '')}>
-            B
-          </NavLink>
-        </li>
-        <li className="w-24 font-semibold text-gray-800 hover:text-blue-500">
-          <NavLink to={ROUTE.SIGN_UP} className={({ isActive }) => (isActive ? 'text-blue-500' : '')}>
-            SIGN_UP
-          </NavLink>
-        </li>
-        <li className="w-24 font-semibold text-gray-800 hover:text-blue-500">
-          <NavLink to={ROUTE.LOGIN} className={({ isActive }) => (isActive ? 'text-blue-500' : '')}>
-            LOGIN
-          </NavLink>
-        </li>
+        {GNB_LINK.map(({ title, link }) => (
+          <li className="w-24 font-semibold text-gray-800 hover:text-blue-500">
+            <NavLink to={link} className={({ isActive }) => (isActive ? 'text-blue-500' : '')}>
+              {title}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   )
--- client/src/constants/route.constant.ts
+++ client/src/constants/route.constant.ts
@@ -5,3 +5,11 @@ export const ROUTE = {
   A: '/a',
   B: '/b',
 }
+
+export const GNB_LINK = [
+  { title: 'HOME', link: ROUTE.HOME },
+  { title: 'A', link: ROUTE.A },
+  { title: 'B', link: ROUTE.B },
+  { title: 'LOGIN', link: ROUTE.LOGIN },
+  { title: 'SIGN_UP', link: ROUTE.SIGN_UP },
+]
