30eb0269a70bc1ae64cc81de591c5b50affef501
jihyunglee <jhlee7125@gmail.com>
2023-03-06 15:50:37 +0900

    feat: 로그인 기능 구현 전 기본 라우터 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import StyleApp from './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Login from './pages/Login';
+import A from './pages/A';
+import B from './pages/B';
+import C from './pages/C';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import StyleApp from './App.module.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className={StyleApp.App}>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route element={<Outlet />}>
+          <Route path='/a' element={<A />} />
+          <Route path='/b' element={<B />} />
+          <Route path='/c' element={<C />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
