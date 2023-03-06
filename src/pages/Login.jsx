f851cd6048995c2e4f0e23e981234ee8821bb96e
jihyunglee <jhlee7125@gmail.com>
2023-03-06 17:08:57 +0900

    feat: 로그인 기본 기능 구현
    1. input 입력값 username, password에 저장
    2. 로그인 버튼 클릭 시 username, password 정상적으로 값 전달되는지 log로 확인
    3. 최초 페이지 렌더링 시 username에 focusing이 되도록 설정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,55 @@
+import { useEffect, useRef, useState } from 'react';
 import StyleApp from '../App.module.css';
 
 const Login = () => {
+  const inputRef = useRef(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(username, password);
+  };
+
+  const handleChange = (e) => {
+    if (e.target.id === 'username') {
+      setUsername(e.target.value);
+    } else if (e.target.id === 'password') {
+      setPassword(e.target.value);
+    }
+  };
+
   return (
     <article className={StyleApp.login}>
       <h2 className={StyleApp.loginTitle}>로그인 해주세요.</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
-          <legend className={StyleApp.ir}>로그인</legend>
+          <legend className={StyleApp.ir}>로그인 - 사용자 이름</legend>
           <label htmlFor='username'>사용자 이름</label>
-          <input id='username' type='text' required />
+          <input
+            id='username'
+            type='text'
+            required
+            ref={inputRef}
+            onChange={handleChange}
+          />
         </fieldset>
         <fieldset>
-          <legend className={StyleApp.ir}>로그인</legend>
+          <legend className={StyleApp.ir}>로그인 - 비밀번호</legend>
           <label htmlFor='password'>비밀번호</label>
-          <input id='password' type='password' required />
+          <input
+            id='password'
+            type='password'
+            required
+            onChange={handleChange}
+          />
         </fieldset>
+        <button>로그인</button>
       </form>
     </article>
   );
