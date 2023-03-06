e5a33b89eed62a1a621058c93bdbdab3bef9de76
jihyunglee <jhlee7125@gmail.com>
2023-03-06 18:49:40 +0900

    feat: LoginProvider의 하위 컴포넌트로 App을 설정하여 isLogin 접근 허용

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { LoginProvider } from './context/LoginContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LoginProvider>
+      <App />
+    </LoginProvider>
   </React.StrictMode>
 );
 
