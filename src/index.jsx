17a1c0be1ac1ebafe52427c97af98ef510e1148b
jihyunglee <jhlee7125@gmail.com>
2023-03-06 18:00:16 +0900

    refactor: 로그인 여부에 따라 페이지 렌더링에 변화를 주기 위해 라우터 설정 변경

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
