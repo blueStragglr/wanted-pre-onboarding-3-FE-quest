f80ca99f40a66ae2c98fc5269764be834309c66b
admin <admin@adminui-MacBookAir.local>
2023-03-06 23:59:18 +0900

    init project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function App() {
+  function onClick() {
+    alert("Hello World!");
+  }
+
+  return (
+    <div className="flex h-screen w-screen justify-center items-center">
+      <button
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4"
+        onClick={onClick}
+      >
+        Click!
+      </button>
+    </div>
+  );
+}
+
+export default App;
