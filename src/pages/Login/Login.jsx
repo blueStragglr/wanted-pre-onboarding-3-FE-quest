d6233c865cca87f38d1849c4ad9447a84f202cb7
jihyunglee <jhlee7125@gmail.com>
2023-03-06 18:53:43 +0900

    feat: 로그인 페이지 스타일시트 파일 import 변경

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState, useContext } from 'react';
-import { LoginContext } from '../context/LoginContext';
+import { LoginContext } from '../../context/LoginContext';
 import { useNavigate } from 'react-router-dom';
-import StyleApp from '../App.module.css';
+import StyleLogin from './Login.module.css';
 
 const Login = () => {
   const inputRef = useRef(null);
@@ -33,11 +33,11 @@ const Login = () => {
   };
 
   return (
-    <article className={StyleApp.login}>
-      <h2 className={StyleApp.loginTitle}>로그인 해주세요.</h2>
+    <article className={StyleLogin.login}>
+      <h2 className={StyleLogin.loginTitle}>로그인 해주세요.</h2>
       <form onSubmit={handleSubmit}>
         <fieldset>
-          <legend className={StyleApp.ir}>로그인 - 사용자 이름</legend>
+          <legend className={StyleLogin.ir}>로그인 - 사용자 이름</legend>
           <label htmlFor='username'>사용자 이름</label>
           <input
             id='username'
@@ -48,7 +48,7 @@ const Login = () => {
           />
         </fieldset>
         <fieldset>
-          <legend className={StyleApp.ir}>로그인 - 비밀번호</legend>
+          <legend className={StyleLogin.ir}>로그인 - 비밀번호</legend>
           <label htmlFor='password'>비밀번호</label>
           <input
             id='password'
