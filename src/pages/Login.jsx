368e207a50819a139173eea5933f468ff4617ad9
jihyunglee <jhlee7125@gmail.com>
2023-03-06 16:46:39 +0900

    design: 로그인 페이지 기본 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import StyleApp from '../App.module.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <article className={StyleApp.login}>
+      <h2 className={StyleApp.loginTitle}>로그인 해주세요.</h2>
+      <form>
+        <fieldset>
+          <legend className={StyleApp.ir}>로그인</legend>
+          <label htmlFor='username'>사용자 이름</label>
+          <input id='username' type='text' required />
+        </fieldset>
+        <fieldset>
+          <legend className={StyleApp.ir}>로그인</legend>
+          <label htmlFor='password'>비밀번호</label>
+          <input id='password' type='password' required />
+        </fieldset>
+      </form>
+    </article>
+  );
 };
 
 export default Login;
