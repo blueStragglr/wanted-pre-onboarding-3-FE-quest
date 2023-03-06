c265ed51d68693302a68f6c5942fd296fa5833a0
jiwonmik <jiwonmik@gmail.com>
2023-03-06 15:54:30 +0900

    feat: Routing Signin and Signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import Header from './components/layouts/Header';
+import { Toast } from './components/auth/styles';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 
 function App() {
@@ -6,6 +8,7 @@ function App() {
     <>
       <Header />
       <Sidebar />
+      <Toast />
     </>
   );
 }
