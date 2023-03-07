c498e0171e74d6077f7055a86cac802da93c80d6
Alexis1226 <feuri41@gmail.com>
2023-03-07 21:26:07 +0900

    lesson 1 : Router활용

--- assignment/src/components/Layout.jsx
+++ assignment/src/components/Layout.jsx
@@ -1,8 +1,15 @@
 import Aside from "./Aside";
 import "./Layout.css";
 
-function Layout(props) {
-  const { children } = props;
+function Layout({ children, withLogin }) {
+  const { isLogged, routeToLoginPage } = useLoginState();
+
+  // 로그인되지 않은 경우에는 로그인 페이지로 리다이렉트
+  if (!isLogged && withLogin) {
+    routeToLoginPage();
+    // 라우팅 괴기 전 잠깐이라도 로그인이 필요한 컨텐츠를 보여주지 않음
+    return <>로그인 페이지로 이동합니다..</>;
+  }
 
   return (
     <section id="layout">
---
+++ assignment/src/utils/pageWithoutLogin.tsx
@@ -0,0 +1,5 @@
+const PageWithoutLogin = () => {
+  return <div>{/* // 로그인이 필요 없는 페이지 */}</div>;
+};
+
+export default PageWithoutLogin;
