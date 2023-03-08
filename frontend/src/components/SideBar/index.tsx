7b73ce98940655345f975edf38c48dd8b358b46f
hwinkr <dnddl8280@naver.com>
2023-03-08 23:25:13 +0900

    Edit: 사이드바 페이지 리스트로 수정

--- frontend/src/components/SideBar/index.tsx
+++ frontend/src/components/SideBar/index.tsx
@@ -1,24 +1,18 @@
 import { useNavigate, useParams } from "react-router-dom"
-import { useRecoilState, useRecoilValue } from "recoil";
-import { pageState } from "../../util/state/atom";
+import { getSidebarItem } from "../../Router";
 import AddPage from "../AddPage";
 import { Wrapper, Item, Line } from "./style"
 
+interface ISidebarItem{
+    path:string;
+    element:React.ReactNode;
+    label:string;
+}
+
 const SideBar = () => {
-    const defaultPages = ['a', 'b', 'c']
     const navigate = useNavigate();
-    const [pages, setPages] = useRecoilState(pageState)
     const handlePage = (currPage:string):void => {
         navigate(`/page/${currPage}`)
-    }
-    const deletePage = (index:number):void => {
-        if(window.confirm('이 페이지를 삭제 하겠습니까?')){
-            setPages((prevPages) => {
-                const copyPrev = [...prevPages]
-                copyPrev.splice(index, 1)
-                return copyPrev
-            })
-        }
     }
     const {pageId} = useParams();
 
@@ -26,16 +20,16 @@ const SideBar = () => {
     <>
     <Wrapper>   
         <AddPage />
-        {pages.map((item, index)=> (
-            <Item 
-            key = {index}
-            onClick = {() => handlePage(item)}
-            isActive = {pageId === item}>
-                <span>{`page ${item.toUpperCase()}`}</span>
-                {defaultPages.indexOf(item) === -1 
-                ? <span onClick={() => deletePage(index)}>❌</span>
-                : null}
-            </Item>))}
+        {
+            getSidebarItem.map((item:ISidebarItem, index:number) => (
+                <Item 
+                key={index}
+                onClick={()=>handlePage(item.label)}
+                isActive={pageId === item.label}>
+                    <span>{`page ${item.label.toUpperCase()}`}</span>
+                </Item>
+            ))
+        }
     </Wrapper>
     <Line />
     </>
