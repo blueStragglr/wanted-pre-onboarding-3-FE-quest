18cc4ae3067e5550e4de9efa6de89e65b6f3b640
hwinkr <dnddl8280@naver.com>
2023-03-08 23:24:45 +0900

    Add: 로그아웃 버튼 추가

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,17 +1,32 @@
 import { useNavigate } from "react-router-dom"
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { loginState } from "../../util/state/atom";
 import { Line, Title, Wrapper } from "./style"
 
 const Header = () => {
   const navigate = useNavigate();
+  const [{isLoggedIn ,username}, setLoggedIn] = useRecoilState(loginState)
   const handleLogin = () => navigate('/auth/login')
+  const handleLogout = () => {
+    if(!window.confirm('로그아웃 하시겠습니까?')){
+      return
+    }
+    setLoggedIn({isLoggedIn : false, username:''});
+    navigate('/')
+  }
   return (
     <>
         <Wrapper>
             <Title>
                 Wanted Pre-onboaring course
             </Title>
-            <button
-            onClick={handleLogin}>Login</button>
+            {isLoggedIn 
+            ? <>
+              <span>{username}</span>
+              <button onClick={handleLogout}>Logout</button>            
+              </>
+            : <button onClick={handleLogin}>Login</button>            
+            }
         </Wrapper>
         <Line/>
     </>
