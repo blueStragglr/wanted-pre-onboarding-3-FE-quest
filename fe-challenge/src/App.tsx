1f920fbb01b0cbdde5c798b088223a5053766b69
Jinnie <hjk7639@gmail.com>
2023-03-08 14:01:16 +0900

    라우터 페이지들 배열로 정리하기

--- fe-challenge/src/App.tsx
+++ fe-challenge/src/App.tsx
@@ -6,8 +6,51 @@ import PageC from './Pages/PageC';
 import Login from './Pages/Login/Login';
 import reset from './Style/style';
 import { Global } from '@emotion/react';
+import { ComponentType } from 'react';
+
+interface RouterItem {
+  path: string;
+  component: ComponentType<any>;
+  withAuthorization: boolean;
+  label: string;
+  children?: RouterItem[];
+}
 
 function App() {
+  const RouterInfo: RouterItem[] = [
+    {
+      path: '/login',
+      component: Login,
+      withAuthorization: false,
+      label: '로그인',
+    },
+    {
+      path: '/',
+      component: PageA,
+      withAuthorization: true,
+      label: 'MainPage',
+      children: [
+        {
+          path: '/pagea',
+          component: PageA,
+          withAuthorization: true,
+          label: 'PageA',
+        },
+        {
+          path: '/pageb',
+          component: PageB,
+          withAuthorization: true,
+          label: 'PageB',
+        },
+        {
+          path: '/pagec',
+          component: PageC,
+          withAuthorization: true,
+          label: 'PageC',
+        },
+      ],
+    },
+  ];
   return (
     <>
       <Global styles={reset} />
