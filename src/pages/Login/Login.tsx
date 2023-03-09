547be8930aae25ff4711fdd7f81ab63c75d0df90
Yena-Yun <cheryleduart@gmail.com>
2023-03-09 14:42:37 +0900

    add Login view

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+import { UserInfo } from 'types/user';
 import styles from './Login.module.scss';
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [userInfo, setUserInfo] = useState<UserInfo>({ name: '' });
+
+  const loginSubmitHandler = async () => {};
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Login with Mock API</h1>
+      <form className={styles.form} onSubmit={loginSubmitHandler}>
+        <label className={styles.label}>
+          Username:
+          <input className={styles.input} type='text' name='username' />
+        </label>
+        <label className={styles.label}>
+          Password:
+          <input className={styles.input} type='password' name='password' />
+        </label>
+        {/* submit 버튼 */}
+        <button className={styles.submitButton} type='submit'>
+          Submit
+        </button>
+      </form>
+      <div>
+        <h2>User info</h2>
+        {/* useState의 userInfo를 렌더링 */}
+        {JSON.stringify(userInfo)}
+      </div>
+    </div>
+  );
 };
