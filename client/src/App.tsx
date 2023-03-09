e2116fdae289fc57fab416a4ff0dc98b16e90cac
yunho <kimyouknow@naver.com>
2023-03-09 21:51:40 +0900

    Build: CRA, gh-pages 배포 에러 해결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import Router from '@/router'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Router />
     </BrowserRouter>
   )
