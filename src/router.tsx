172bbd19b4f6838b70545ea3410b1c6ea07d1f39
soralee <sora2821@gmail.com>
2023-03-09 18:29:02 +0900

    Feat: add sidebarContent from routerInfo

--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,84 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { Root, Error, PageA, PageB, PageC } from '@/pages';
+import { Authorization } from '@/components/Authorization';
+import { Root, Error, PageA, PageB, PageC, Login } from '@/pages';
 
-export const router = createBrowserRouter([
+interface RouterItem {
+  path: string;
+  element: React.ReactNode;
+  withAuthorization: boolean;
+  label: string;
+  errorElement: React.ReactNode;
+}
+
+interface SidebarItem {
+  path: string;
+  label: string;
+}
+
+const RouterInfo: RouterItem[] = [
   {
     path: '/',
     element: <Root />,
+    withAuthorization: false,
+    label: 'home',
     errorElement: <Error />,
-    children: [
-      {
-        path: 'pageA',
-        element: <PageA />,
-        errorElement: <Error />,
-      },
-      {
-        path: 'pageB',
-        element: <PageB />,
-        errorElement: <Error />,
-      },
-      {
-        path: 'pageC',
-        element: <PageC />,
-        errorElement: <Error />,
-      },
-    ],
   },
-]);
+  {
+    path: '/login',
+    element: <Login />,
+    withAuthorization: false,
+    label: 'login',
+    errorElement: <Error />,
+  },
+  {
+    path: '/pageA',
+    element: <PageA />,
+    withAuthorization: true,
+    label: 'Page A',
+    errorElement: <Error />,
+  },
+  {
+    path: '/pageB',
+    element: <PageB />,
+    withAuthorization: true,
+    label: 'Page B',
+    errorElement: <Error />,
+  },
+  {
+    path: '/pageC',
+    element: <PageC />,
+    withAuthorization: true,
+    label: 'Page C',
+    errorElement: <Error />,
+  },
+];
+
+export const router = createBrowserRouter(
+  RouterInfo.map((routerInfo) => {
+    return routerInfo.withAuthorization
+      ? {
+          path: routerInfo.path,
+          element: <Authorization>{routerInfo.element}</Authorization>,
+          errorElement: <Error />,
+        }
+      : {
+          path: routerInfo.path,
+          element: routerInfo.element,
+          errorElement: <Error />,
+        };
+  }),
+);
+
+export const SidebarContent: SidebarItem[] = (() =>
+  RouterInfo.reduce((prev, current) => {
+    if (current.withAuthorization) {
+      return [
+        ...prev,
+        {
+          path: current.path,
+          label: current.label,
+        },
+      ];
+    }
+    return prev;
+  }, [] as SidebarItem[]))();
