b71510def9869a02a45348b561988493f8464c87
Yena-Yun <cheryleduart@gmail.com>
2023-03-09 14:23:03 +0900

    add more codes for absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [react(), tsconfigPaths(), svgrPlugin()],
 });
