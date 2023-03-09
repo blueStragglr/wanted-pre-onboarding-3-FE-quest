ee90760d19ddae75008c265811771a983db250f5
Yena-Yun <cheryleduart@gmail.com>
2023-03-09 14:41:36 +0900

    remove some absolute path logic

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,5 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '~': resolve(__dirname, 'src'),
-    },
-  },
   plugins: [react(), tsconfigPaths(), svgrPlugin()],
 });
