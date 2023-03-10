3592da3dc617565c57e807c2398ec164ddd5a454
hwinkr <dnddl8280@naver.com>
2023-03-11 00:27:56 +0900

    Edit : 유저 타입 수정

--- frontend/src/util/users/users.ts
+++ frontend/src/util/users/users.ts
@@ -1,26 +1,7 @@
-export interface IUserInfo{
-    username:string;
-}
+export type IUserInfo = string;
 
 export interface IUser{
-    username:string;
-    password:string;
-    userInfo:IUserInfo
+    username: string;
+    password: string;
+    userInfo: IUserInfo
 }
-
-export const users:IUser[] = [
-    {
-        username : 'woong123',
-        password : 'qwer1234!',
-        userInfo : {
-            username : 'woong'
-        }
-    },
-    {
-        username : 'choi99',
-        password : 'qwer1234!',
-        userInfo : {
-            username : 'choi'
-        }
-    },
-]
