cb487089781107fdb5382b4372eea3885f3cc34e
hwinkr <dnddl8280@naver.com>
2023-03-11 00:23:32 +0900

    Add : jwt / login 로그인 버튼 추가

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,19 +1,25 @@
 import { useNavigate } from "react-router-dom"
 import { useRecoilState, useSetRecoilState } from "recoil";
+import { ACCESS_TOKEN_KEY } from "../../util/api/const";
 import { loginState } from "../../util/state/atom";
 import { Line, Title, Wrapper } from "./style"
 
 const Header = () => {
   const navigate = useNavigate();
-  const [{isLoggedIn ,username}, setLoggedIn] = useRecoilState(loginState)
-  const handleLogin = () => navigate('/auth/login')
+  const [{isLoggedIn ,name}, setLoggedIn] = useRecoilState(loginState)
+
+  const handleLogin = (type:string) => navigate(`/auth/login/${type}`)
   const handleLogout = () => {
     if(!window.confirm('로그아웃 하시겠습니까?')){
       return
     }
-    setLoggedIn({isLoggedIn : false, username:''});
+    setLoggedIn({isLoggedIn : false, name:''});
+    if(localStorage.getItem(ACCESS_TOKEN_KEY) !== undefined) {
+      localStorage.removeItem(ACCESS_TOKEN_KEY)
+    }
     navigate('/')
   }
+
   return (
     <>
         <Wrapper>
@@ -22,10 +28,14 @@ const Header = () => {
             </Title>
             {isLoggedIn 
             ? <>
-              <span>{username}</span>
+              <span>{name}</span>
               <button onClick={handleLogout}>Logout</button>            
               </>
-            : <button onClick={handleLogin}>Login</button>            
+            : <>
+                <button onClick={() => handleLogin('jwt')}>JWT Login</button>
+                <button onClick={() => handleLogin('local')}>LOCAL Login</button>            
+              </>
+            
             }
         </Wrapper>
         <Line/>
