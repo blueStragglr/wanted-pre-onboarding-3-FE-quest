13dafdf6e11202f1fb2d3c1c686b817ab210e2cc
hwinkr <dnddl8280@naver.com>
2023-03-12 14:03:16 +0900

    Edit: Home page 컴포넌트 수정

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,12 +1,10 @@
 import SideBar from '../../components/SideBar'
 import Detail from '../Detail'
 import { Wrapper } from './style'
-import Header from '../../components/Header'
 
 const Home = () => {
   return (
     <>
-    <Header />
     <Wrapper>
       <SideBar />
       <Detail />
